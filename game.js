022d3ada8c4babc54b584fdc797a54d3c508457f
zarigata <cpuvalle@gmail.com>
2025-04-23 15:48:30 -0300

    Add auto-start race functionality when game loads

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ window.addEventListener('load', () => {
     audio.init();
     gameLogic.init();
     ui.init();
+    // CODEX: auto-start race immediately
+    ui.startRace();
     tracks.generate();
     // Start background music
     audio.playMIDI('music/track1.mid');
